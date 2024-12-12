28bcb46c4c2c537e18bb3ce6d61a9ccae7645978
Ligo_code <rigatovt@gmail.com>
2024-12-12 16:19:59 -0500

    asyncWrapper created

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
+
 const connectDB = (url) => {
-return mongoose.connect(url,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
+  return mongoose.connect(url)
 }
 
-module.exports = connectDB
-
-  
+module.exports = connectDB;
