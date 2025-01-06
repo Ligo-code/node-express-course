a8be6156f9153926e79a7c229aa33c88aaef090c
Ligo_code <rigatovt@gmail.com>
2025-01-06 12:21:05 -0500

    Implemented JWT authentication as per video tutorial

--- 05-JWT-Basics/starter/middleware/not-found.js
+++ 05-JWT-Basics/starter/middleware/not-found.js
@@ -1,3 +1,16 @@
-const notFound = (req, res) => res.status(404).send('Route does not exist')
-
-module.exports = notFound
+const notFoundMiddleware = (req, res) => {
+    console.warn("Route not found:", req.originalUrl);
+    res.status(404).json({ msg: "Route not found" });
+  };
+  
+  module.exports = notFoundMiddleware;
+  
+  // errors/custom-error.js
+  class CustomAPIError extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
+    }
+  }
+  
+  module.exports = CustomAPIError;
